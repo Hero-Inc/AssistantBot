bdac633df35d5f51faac0b054eb9bbf8fec6ecb6
Jack Footner <jackf@pac.internal>
2017-05-26 14:06:12 +0930

    Use Eris, First major

--- index.js
+++ index.js
@@ -1,80 +1,109 @@
-const Discord = require('discord.js');
+const Discord = require('eris');
 const GoogleAssistant = require('google-assistant');
 const Snowboy = require('snowboy');
-// const ffmpeg = require('fluent-ffmpeg');
-const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
+const stream = require('stream');
 
-
-var detector = new Snowboy.Detector();
 var models = new Snowboy.Models();
-
 models.add({
-	file: '',
+	file: 'okgoogle.pmdl',
 	sensitivity: 0.5,
-	hotwords: '',
+	hotwords: 'OK Google',
 });
 
-var bot = new Discord.Client();
-var assistant = new GoogleAssistant({
-	auth: {
-		keyFilePath: '',
-		savedTokensPath: '',
-	},
-	audio: {
-		encodingIn: 'FLAC',
-		sampleRateOut: 24000,
+var bot = new Discord.CommandClient('TOKEN', {
+	description: 'Your personal assistant, running on Discord Eris and Google Assistant.',
+	defaultCommandOptions: {
+		caseInsensitive: true,
+		deleteCommand: true,
+		guildOnly: true,
 	},
 });
-
-detector
-	.on('silence', () => {
-		console.log('silence');
-	})
-	.on('sound', buffer => {
-		console.log('sound');
-	})
-	.on('hotword', (index, hotword, buffer) => {
-		console.log('hotword', index, hotword);
-	})
-	.on('error', () => {
-		console.log('error');
-	});
 
 bot
-	.on('msg', (msg) => {
-		if (msg.author.bot) return;
-		if (msg.channel.type !== 'text') return msg.reply('Sorry, I only work in text channels').then(m => { m.delete(10000); });
-		// Join a voiceChannel
-		if (msg.content.toLowerCase() === 'gjoin' && msg.member.voiceChannel !== undefined) {
-			msg.member.voiceChannel.join()
-			.then(connection => {
-				msg.reply(`OK, Joining ${msg.member.voiceChannel}`)
-					.then(m => {
-						m.delete(10000);
-					});
-			}, e => {
-				msg.reply(`Error joining your channel: ${e}`)
-					.then(m => {
-						m.delete(10000);
-					});
-			});
-			msg.delete(10000);
-		} else if (msg.content.toLowerCase() === 'ghelp') {
-			msg.reply("I'm your personal assistant, use 'gjoin' to make me join your channel");
-		}
+	.on('error', e => {
+		console.log(e);
 	});
 
-assistant
-	.on('ready', () => {
-		bot.login('AUTHKEY');
-	})
-	.on('started', conversation => {
-		let file = fs.createWriteStream('stuff');
-		conversation
-			.on('data', data => {
-				file.write(data);
-			});
-	})
-	.on('error', e => {
-		throw e;
-	});
+bot.registerCommand(
+	'join',
+	(msg, args) => {
+		if (msg.member.voiceState.channelID !== undefined) {
+			bot.joinVoiceChannel(msg.member.voiceState.channelID)
+				.then(conn => {
+					let receive = conn.receive('opus');
+					let detector = new Snowboy.Detector();
+					let assistant = new GoogleAssistant({
+						auth: {
+							keyFilePath: '',
+							savedTokensPath: '',
+						},
+						audio: {
+							encodingIn: 'FLAC',
+							sampleRateOut: 24000,
+						},
+					});
+					let assisting = false;
+					let ready = false;
+					let pass = new stream.PassThrough();
+					let speaking = false;
+					assistant
+						.on('audio-data', data => {
+							pass.write(data);
+							if (!speaking) {
+								conn.play(pass);
+								speaking = true;
+							}
+						})
+						.on('ready', () => {
+							ready = true;
+						})
+						.on('started', conversation => {
+							assisting = true;
+							conversation
+								.on('end-of-utterance', () => {
+									receive.end();
+								})
+								.on('transcription', text => {
+									console.log(`Transcript: ${text}`);
+								})
+								.on('ended', (err, continueConversation) => {
+									if (err) {
+										console.log(err);
+									} else if (continueConversation) {
+										receive = conn.receive('opus');
+										assistant.start();
+									} else {
+										assisting = false;
+										receive = conn.receive('opus');
+										receive.pipe(detector);
+									}
+								})
+								.on('error', e => {
+									console.log(e);
+								});
+							// Send user data to the conversation
+							ffmpeg(receive)
+								.audioCodec('flac')
+								.audioBitrate(24000)
+								.audioChannels(1)
+								.pipe(conversation);
+						});
+					detector
+						.on('hotword', (index, hotword, buffer) => {
+							if (!assisting && ready) {
+								assistant.start();
+							}
+							console.log('hotword', index, hotword);
+						});
+					receive.pipe(detector);
+				});
+		}
+		bot.createMessage(msg.channel.id, 'Hello');
+	},
+	{
+		aliases: ['voice', 'come'],
+		description: 'Join the voice channel',
+		fullDescription: 'Make the bot join the voice channel the user is currently connected to, if any.',
+	}
+);
